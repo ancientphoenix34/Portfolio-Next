ea32d84b6751fc11faf3f63cbecc411a1305a91f
Farseen <farsukoothradan@gmail.com>
2026-05-12 16:06:51 +0530

    theme provider implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "@/components/Hoc/Provider";
 
 const font=Inter({
   weight:['100','200','300','400','500','600','700','800','900'],
@@ -19,8 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
-      <body  className={`${font.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body  className={`${font.className} antialiased`}>
+        <Provider>
+        {children}
+        </Provider>
+        </body>
     </html>
   );
 }
